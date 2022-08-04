b3a67373300e8a82d199cd8d131edf565bee9259
othmanjabes <k11111t12@gmail.com>
2022-08-04 20:47:39 +0300

    Some comments have been added to the code

--- Data_Structure_1/finel.js
+++ Data_Structure_1/finel.js
@@ -1,16 +1,21 @@
 function parseText(text,caseSensitive,punc){
-    let arr;
+    let arr; //this array we start them
+    let finel = []; // we concat every new element to this array
+    
     // for remove the ' ' && '.'
     if(punc === true)
     {
+        //if true remeve space and point | you can add any char you want remove
         arr = text.split(/[. ]+/);
     }else{
+        //if false splite only by white space and return array
         arr = text.split(' ');
     }
-    //
-    const newArr = [].concat(arr);
-    let finel = [];
-    let how_many_time_is_apper = 0;
+    // End the (punc) if statment
+
+    let newArr = [].concat(arr);
+    let how_many_time_is_apper = 0; //count how many time the word is apper, after this return to zero value.
+    
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
             if(caseSensitive){
@@ -42,11 +47,22 @@ function parseText(text,caseSensitive,punc){
             // [['word1',count],['word2',count],['word3',count]]
             //finel.push(temp); //enable, that converte to 2D
         }
-        how_many_time_is_apper = 0;
+        how_many_time_is_apper = 0; //
     }
 
     //remove replace word if A or a
-    if(caseSensitive === true){
+    /*
+    If a word appears twice with a capital letter and once with a lowercase letter,
+    no word will be deleted, but the program will count the two and give the value to these two elements
+    example below:
+    1. before work this function
+    ['He', 2, 'saw', 1, 'her', 1, 'walking,', 1, 'so', 1, 'he', 2, 'asked', 1, 'her.', 1]
+    2. after run this function
+    ['He', 2, 'saw', 1, 'her', 1, 'walking,', 1, 'so', 1, 'asked', 1, 'her.', 1]
+    === see ['He', 2] & ['he', 2] and think what the diferent
+    */
+
+    if(caseSensitive){
         for (let i = 0; i < finel.length-1; i++) {
             let c = 0;
             for (let j = 0; j < finel.length-1; j++) {
@@ -55,10 +71,11 @@ function parseText(text,caseSensitive,punc){
                 if(sI === sJ && isNaN(sI) && isNaN(sJ)){
                     c++;
                     if(c > 1){
-                        finel.splice(i,2);
+                        finel.splice(j,2);
                     }
                 }
             }
+            c = 0;
         }
     }
 
---
+++ script1.js
@@ -0,0 +1,1 @@
+// Main files to write & other.
