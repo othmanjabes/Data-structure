d7790a0a727a104a53537230a948d2263f7da81b
othmanjabes <k11111t12@gmail.com>
2022-08-06 18:40:45 +0300

    clean code and command

--- Data_Structure_1/finel.js
+++ Data_Structure_1/finel.js
@@ -12,83 +12,42 @@ punc: true:          'her,' ≠ 'her | false: 'her,' == 'her'
 */
 function parseText(text,caseSensitive,punc){
     let arr; //this array we start them
-    let finel = []; // we concat every new element to this array
+    let arrayFinel_1D = []; // save node(word,count) in 1D array
+    
     let how_many_time_is_apper = 0; //count how many time the word is apper, after this return to zero value.
     
-    // for remove the ' ' && '.'
-    if(punc)
-    {
-        //if true remeve space and point | you can add any char you want remove
-        arr = text.split(/[. ]+/);
-    }else{
-        //if false splite only by white space and return array
-        arr = text.split(' ');
-    }
+    punc ? arr = text.split(/[. ]+/) :arr = text.split(' ');
 
-    //1. count the number of times the word appears
-    //2. added the count & element -> to the finel Array
+    // - save the word we want to save, save the count the number of times the word appears
+    let indexOfLastWord;
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
-                if(arr[i].toUpperCase() === arr[j].toUpperCase() && caseSensitive){
+                if(arr[i].toUpperCase() === arr[j].toUpperCase() && caseSensitive || arr[i] === arr[j]){
                         how_many_time_is_apper++;
+                        indexOfLastWord = j;
                 }
-                else if(arr[i] === arr[j])
-                {
-                        how_many_time_is_apper++;
-                    }
             }
 
-        // add the element & count to the finel Array
-        if(!finel.includes(arr[i]) && arr[i] !== '')
+        // add the element & count to the arrayFinel_1D
+        if(!arrayFinel_1D.includes(arr[i]) && arr[i] !== '')
         {
-            finel = finel.concat(arr[i],how_many_time_is_apper);
+            arrayFinel_1D = arrayFinel_1D.concat(arr[indexOfLastWord],how_many_time_is_apper);
         }
         how_many_time_is_apper = 0; 
     }
-
-    //This function only works if the user requests
-    //Delete repeated words, whether they are uppercase or lowercase
-    if(caseSensitive){
-        /*
-        If a word appears twice with a capital letter and once with a lowercase letter,
-        no word will be deleted, but the program will count the two and give the value to these two elements
-        example below:
-        1. before work this function
-        ['He', 2, 'saw', 1, 'her', 1, 'walking,', 1, 'so', 1, 'he', 2, 'asked', 1, 'her.', 1]
-        2. after run this function
-        ['He', 2, 'saw', 1, 'her', 1, 'walking,', 1, 'so', 1, 'asked', 1, 'her.', 1]
-        === see ['He', 2] & ['he', 2] and think what the diferent
-        */
-        for (let i = 0; i < finel.length-1; i++) {
-            let c = 0;
-            for (let j = 0; j < finel.length-1; j++) {
-                let sI = finel[i].toString().toUpperCase();
-                let sJ = finel[j].toString().toUpperCase();
-                if(sI === sJ && isNaN(sI) && isNaN(sJ)){
-                    c++;
-                    if(c > 1){
-                        finel.splice(j,2);
-                    }
-                }
-            }
-            c = 0;
-        }
-    }
-
+    
     //converte from 1D to 2D array
-    let arr2D = [];
-    for (let i = 0; i < finel.length; i+=2) {
-        arr2D.push(Array.of(finel[i],finel[i+1]));
+    let arrayFinel_2D = [];
+    for (let i = 0; i < arrayFinel_1D.length; i+=2) {
+        arrayFinel_2D.push(Array.of(arrayFinel_1D[i],arrayFinel_1D[i+1]));
     }
-    return arr2D; //return the finel Array -> (result)
+    return arrayFinel_2D; //return 2D Array -> (result)
 }
 
 //sort the array by first column
 function sort2D(arr){
     return arr.sort((a, b) => a[0].localeCompare(b[0]));
 }
-
-
 
 
 
--- Data_Structure_1/withoutslash.js
+++ Data_Structure_1/withoutslash.js
@@ -1,52 +1,32 @@
-//main function
-/*
-input:  "He saw her walking, so he asked her."
-parseText(text,false,false)
-output: [['He', 1],['saw', 1],['her', 1],['walking,', 1],['so', 1],['he', 1],['asked', 1],['her.', 1]]
-*/
-
-/* 
-text:                'Hellow world!' -> ['Hellow','world!']
-caseSensitive: true: 'He' ≠ 'he' | false 'He' == 'he'
-punc: true:          'her,' ≠ 'her | false: 'her,' == 'her'
-*/
 function parseText(text,caseSensitive,punc){
-    let arr; //this array we start them
-    let finel = []; // we concat every new element to this array
-    let how_many_time_is_apper = 0; //count how many time the word is apper, after this return to zero value.
+    let arr; 
+    let arrayFinel_1D = []; 
+    let arrayFinel_2D = [];
+
+    let how_many_time_is_apper = 0; 
     
     punc ? arr = text.split(/[. ]+/) :arr = text.split(' ');
 
-    //1. count the number of times the word appears
-    //2. added the count & element -> to the finel Array
-    let t;
+    let lastIndexOfLastWord;
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
-                if(arr[i].toUpperCase() === arr[j].toUpperCase() && caseSensitive){
+                if(arr[i].toUpperCase() === arr[j].toUpperCase() && caseSensitive || arr[i] === arr[j]){
                         how_many_time_is_apper++;
-                        t = j;
-                }
-                else if(arr[i] === arr[j])
-                {
-                        how_many_time_is_apper++;
-                        t = j;
+                        lastIndexOfLastWord = j;
                 }
             }
 
-        // add the element & count to the finel Array
-        if(!finel.includes(arr[i]) && arr[i] !== '')
+        if(!arrayFinel_1D.includes(arr[i]) && arr[i] !== '')
         {
-            finel = finel.concat(arr[t],how_many_time_is_apper);
+            arrayFinel_1D = arrayFinel_1D.concat(arr[lastIndexOfLastWord],how_many_time_is_apper);
         }
         how_many_time_is_apper = 0; 
     }
     
-    //converte from 1D to 2D array
-    let arr2D = [];
-    for (let i = 0; i < finel.length; i+=2) {
-        arr2D.push(Array.of(finel[i],finel[i+1]));
+    for (let i = 0; i < arrayFinel_1D.length; i+=2) {
+        arrayFinel_2D.push(Array.of(arrayFinel_1D[i],arrayFinel_1D[i+1]));
     }
-    return arr2D; //return the finel Array -> (result)
+    return arrayFinel_2D; //return the finel Array -> (result)
 }
 
 //sort the array by first column
